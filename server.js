60ea304a6cde2f6be119cffcd48240287522513f
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-11-20 09:41:49 -0800

    added create, delete, and update routes

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
